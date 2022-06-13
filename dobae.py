6738a901c3fceee2ba7b7b5ea878b3d8e4dd6868
leaddwk <63853360+leaddwk@users.noreply.github.com>
2022-06-13 22:53:03 +0900

    Add files via upload

---
+++ dobae.py
@@ -0,0 +1,34 @@
+from time import sleep
+import pyautogui
+
+sleep(3)
+pyautogui.typewrite('dornrrk')
+pyautogui.typewrite('1. ehdgoanfrhk qorentksdl akfmrh ekfgehfhr')
+pyautogui.typewrite('gksmsladl qhdngktk dnflskfk akstp')
+pyautogui.typewrite('anrndghk tkacjsfl ghkfurkdtks')
+pyautogui.typewrite('eogkstkfka eogksdmfh rlfdl qhwjsgktp.')
+pyautogui.press('enter')
+pyautogui.press('enter')
+
+# 2단락
+pyautogui.typewrite('2. skatks dnldp wj thskan cjfrkqdmf enfms emt')
+pyautogui.typewrite('qkfka tjfl qnfqusgkadms dnfl rltkddlftp')
+pyautogui.typewrite('anrndghk tkacjsfl ghkfurkdtks')
+pyautogui.typewrite('eogkstkfka eogksdmfh rlfdl qhwjsgktp.')
+pyautogui.typewrite('3. rkdmf gksmf rhdghkfgksep shvrh rnfma djqtdl')
+pyautogui.press('enter')
+pyautogui.press('enter')
+
+# 3 단락
+pyautogui.typewrite('qkfrdms ekfdms dnflrktma dlfvus ekstladlftp')
+pyautogui.typewrite('anrndghk tkacjsfl ghkfurkdtks')
+pyautogui.typewrite('eogkstkfka eogksdmfh rlfdl qhwjsgktp.')
+pyautogui.typewrite('4. dl rltkdrhk dl akadmfh cndtjddmf ekgkdu')
+pyautogui.typewrite('rhlfhdnsk wmfrjdnsk skfk tkfkdgktp')
+pyautogui.press('enter')
+pyautogui.press('enter')
+
+# 4단락
+pyautogui.typewrite('anrndghk tkacjsfl ghkfurkdtks')
+pyautogui.typewrite('eogkstkfka eogksdmfh rlfdl qhwjsgktp.')
+pyautogui.press('enter')
